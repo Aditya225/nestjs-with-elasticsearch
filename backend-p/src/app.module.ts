2664aa452e797a0f9b913bcfd441a6dd6097fb59
aditya <gauraditya50@gmail.com>
2026-04-13 23:07:51 +0530

    pushed

--- backend-p/src/app.module.ts
+++ backend-p/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ElasticModule } from './elastic/elastic.module';
+// import { LogsModule } from './logs/log.module';
 import { LogsModule } from './logs/log.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
